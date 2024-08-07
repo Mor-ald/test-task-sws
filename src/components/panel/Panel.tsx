02dee38a89bb61588917f72c7560570da56c5711
Mor-ald <mor.ald@yandex.ru>
2024-08-07 15:44:37 +0300

    feature(components): add panel's type, props and jsx

--- src/components/panel/Panel.tsx
+++ src/components/panel/Panel.tsx
@@ -1,8 +1,16 @@
 import styles from "./Panel.module.scss";
 
+type IPanel = {
+	header: string;
+};
+
 /**
  * Panel component
  */
-export default function Panel() {
-	return <></>;
+export default function Panel(props: IPanel) {
+	return (
+		<>
+			<div className={styles.panel}>{props.header}</div>
+		</>
+	);
 }
