024cbff3c37c597a508e9c731211c6ce741252e3
Mor-ald <mor.ald@yandex.ru>
2024-08-07 18:02:24 +0300

    feature(components): add render of NavItems and setActiveNav method

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,8 +1,40 @@
-import styles from "./Navbar.module.css";
+import styles from "./NavBar.module.scss";
+
+import { IconsNames } from "../../ts/enums/IconsNames";
+import navItems from "../../utils/navItems";
+import NavItem from "../navitem/NavItem";
+import { useCallback, useState } from "react";
 
 /**
  * Navbar component
  */
 export default function Navbar() {
-	return <></>;
+	const [navs, setNavs] = useState(navItems);
+
+	const setActiveNav = useCallback(
+		(navName: string) => {
+			const newNavs = [...navs.map((nav) => ({ active: false, navIconName: nav.navIconName as IconsNames, navName: nav.navName }))];
+
+			newNavs.find((nav) => nav.navName === navName)!.active = true;
+			console.log(newNavs);
+			setNavs(newNavs);
+		},
+		[navs, setNavs],
+	);
+
+	return (
+		<>
+			<div className={styles.navbar}>
+				{navs.map((item) => (
+					<NavItem
+						key={item.navName}
+						active={item.active}
+						navIconName={item.navIconName as IconsNames}
+						navName={item.navName}
+						setActiveNav={setActiveNav}
+					/>
+				))}
+			</div>
+		</>
+	);
 }
