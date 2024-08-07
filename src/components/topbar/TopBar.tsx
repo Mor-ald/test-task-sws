8af54361790f23f8ca65b417474ec4f74fc34b5d
Mor-ald <mor.ald@yandex.ru>
2024-08-07 14:03:28 +0300

    feature(components): add topbar's buttons

--- src/components/topbar/TopBar.tsx
+++ src/components/topbar/TopBar.tsx
@@ -1,8 +1,19 @@
+import { IconsNames } from "../../ts/enums/IconsNames";
+import { Button } from "../components";
 import styles from "./TopBar.module.scss";
 
 /**
  * TopBar component
  */
 export default function TopBar() {
-	return <></>;
+	return (
+		<>
+			<div className={styles.topbar}>
+				<div className={styles.buttons}>
+					<Button iconName={IconsNames.Bar} />
+					<Button iconName={IconsNames.ShareArrow} />
+				</div>
+			</div>
+		</>
+	);
 }
