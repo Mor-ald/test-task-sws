b1ebc49ca0f115d1933a0312a9b72ce903ddd635
Mor-ald <mor.ald@yandex.ru>
2024-08-07 14:57:43 +0300

    feature(components): add type, props and tab render

--- src/components/tab/Tab.tsx
+++ src/components/tab/Tab.tsx
@@ -1,8 +1,20 @@
 import styles from "./Tab.module.scss";
 
+type ITab = {
+	active: boolean;
+	tabName: string;
+	setActiveTab: (tabName: string) => void;
+};
+
 /**
  * Tab component
  */
-export default function Tab() {
-	return <></>;
+export default function Tab(props: ITab) {
+	return (
+		<>
+			<div className={props.active ? styles["tab-active"] : styles["tab"]} onClick={() => props.setActiveTab(props.tabName)}>
+				{props.tabName}
+			</div>
+		</>
+	);
 }
