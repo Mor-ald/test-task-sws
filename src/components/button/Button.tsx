8e606c320dc0b81d0de9aeb845cac490be6288df
Mor-ald <mor.ald@yandex.ru>
2024-08-07 13:38:33 +0300

    feature(components): add jsx, props and type for the button

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,8 +1,20 @@
+import { IconsNames } from "../../ts/enums/IconsNames";
 import styles from "./Button.module.scss";
+
+type IButton = {
+	iconName: IconsNames;
+	onClick?: () => void;
+};
 
 /**
  * Button component
  */
-export default function Button() {
-	return <></>;
+export default function Button(props: IButton) {
+	return (
+		<>
+			<div className={styles.button} onClick={props.onClick}>
+				<img src={`../../../public/${props.iconName}.svg`} alt="icon" />
+			</div>
+		</>
+	);
 }
