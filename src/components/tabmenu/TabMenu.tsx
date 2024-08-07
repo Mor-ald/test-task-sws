48be0dd01b05957f2d316b75bf53d0c389a47c81
Mor-ald <mor.ald@yandex.ru>
2024-08-07 14:56:57 +0300

    feature(components): add tabs render, type and setActiveTab method

--- src/components/tabmenu/TabMenu.tsx
+++ src/components/tabmenu/TabMenu.tsx
@@ -1,8 +1,35 @@
+import { useCallback, useState } from "react";
 import styles from "./TabMenu.module.scss";
+import Tab from "../tab/Tab";
+
+type ITabMenu = {
+	tabs: { active: boolean; tabName: string }[];
+};
 
 /**
  * TabMenu component
  */
-export default function TabMenu() {
-	return <></>;
+export default function TabMenu(props: ITabMenu) {
+	const [tabs, setTabs] = useState(props.tabs);
+
+	const setActiveTab = useCallback(
+		(tabName: string) => {
+			const newTabs = [...tabs.map((tab) => ({ active: false, tabName: tab.tabName }))];
+
+			newTabs.find((tab) => tab.tabName === tabName)!.active = true;
+
+			setTabs(newTabs);
+		},
+		[tabs, setTabs],
+	);
+
+	return (
+		<>
+			<div className={styles["tab-menu"]}>
+				{tabs.map((tab) => (
+					<Tab key={tab.tabName} active={tab.active} tabName={tab.tabName} setActiveTab={setActiveTab} />
+				))}
+			</div>
+		</>
+	);
 }
