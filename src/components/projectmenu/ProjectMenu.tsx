70caab87ad70a4810fc6566659eeeb7abeb9cae4
Mor-ald <mor.ald@yandex.ru>
2024-08-07 15:46:48 +0300

    feature(components): add project name and button

--- src/components/projectmenu/ProjectMenu.tsx
+++ src/components/projectmenu/ProjectMenu.tsx
@@ -1,8 +1,21 @@
 import styles from "./ProjectMenu.module.scss";
 
+import { IconsNames } from "../../ts/enums/IconsNames";
+import { Button } from "../components";
+
 /**
  * ProjectMenu component
  */
 export default function ProjectMenu() {
-	return <></>;
+	return (
+		<>
+			<div className={styles["project-menu"]}>
+				<div className={styles["project-name"]}>
+					<span>Название проекта</span>
+					<span>Аббревиатура</span>
+				</div>
+				<Button iconName={IconsNames.ArrowDown} />
+			</div>
+		</>
+	);
 }
