439776e150d414c6dd41616ce8cfbfba15fc61d2
Mor-ald <mor.ald@yandex.ru>
2024-08-07 18:01:21 +0300

    feature(components): add type, props, and render of the nav item

--- src/components/navitem/NavItem.tsx
+++ src/components/navitem/NavItem.tsx
@@ -1,8 +1,23 @@
+import { IconsNames } from "../../ts/enums/IconsNames";
 import styles from "./NavItem.module.scss";
+
+type INavItem = {
+	navIconName: IconsNames;
+	navName: string;
+	active: boolean;
+	setActiveNav: (navName: string) => void;
+};
 
 /**
  * NavItem component
  */
-export default function NavItem() {
-	return <></>;
+export default function NavItem(props: INavItem) {
+	return (
+		<>
+			<div className={props.active ? styles["nav-item-active"] : styles["nav-item"]} onClick={() => props.setActiveNav(props.navName)}>
+				<img src={`../../../public/${props.navIconName}.svg`} alt="nav-icon" />
+				<span>{props.navName}</span>
+			</div>
+		</>
+	);
 }
