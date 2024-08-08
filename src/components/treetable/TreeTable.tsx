0a95e340595aad5309e610f6143b56e5bc260bd5
Mor-ald <mor.ald@yandex.ru>
2024-08-08 16:17:06 +0300

    feature(components): add table in TreeTable component

--- src/components/treetable/TreeTable.tsx
+++ src/components/treetable/TreeTable.tsx
@@ -1,8 +1,32 @@
+import { useState } from "react";
+
 import styles from "./TreeTable.module.scss";
+
+import { TreeRows } from "../treerows/TreeRows";
 
 /**
  * TreeTable component
  */
 export default function TreeTable() {
-	return <></>;
+	const [nodes, setNodes] = useState([]);
+
+	return (
+		<>
+			<table className={styles["tree-table"]}>
+				<thead>
+					<tr>
+						<th className={styles["lvl-col"]}>Уровень</th>
+						<th className={styles["work-col"]}>Наименование работ</th>
+						<th className={styles["salary-col"]}>Основная з/п</th>
+						<th className={styles["equipment-col"]}>Оборудование</th>
+						<th className={styles["expenses-col"]}>Накладные расходы</th>
+						<th className={styles["profit-col"]}>Сметная прибыль</th>
+					</tr>
+				</thead>
+				<tbody>
+					<TreeRows nodes={nodes} />
+				</tbody>
+			</table>
+		</>
+	);
 }
