26a7c05b5a573e92a77e16e35de531b89e082329
Mor-ald <mor.ald@yandex.ru>
2024-08-08 16:17:52 +0300

    feature(pages): add a TreeTable component on the Ciw page

--- src/pages/ciw/Ciw.tsx
+++ src/pages/ciw/Ciw.tsx
@@ -1,8 +1,15 @@
+import { TreeTable } from "../../components/components";
 import styles from "./Ciw.module.scss";
 
 /**
  * Page of construction and installation works
  */
 export default function Ciw() {
-	return <></>;
+	return (
+		<>
+			<div className={styles.ciw}>
+				<TreeTable />
+			</div>
+		</>
+	);
 }
