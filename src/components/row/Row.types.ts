7b28d3dec73ebdfa0c2759c020a40f27af391730
Mor-ald <mor.ald@yandex.ru>
2024-08-10 15:59:12 +0300

    feature(components): add key for onCreateNewRow, onUpdateRow methods

--- src/components/row/Row.types.ts
+++ src/components/row/Row.types.ts
@@ -2,9 +2,10 @@ import { CiwCreateData, CiwUpdateData } from "../../ts/ApiData";
 import { TreeNode } from "../../ts/TreeNode";
 
 export type IRow = {
+	qRowsWithLessNesting: number;
 	node: TreeNode;
 	onAddChildRow: (id: number) => void;
-	onCreateNewRow: (data: CiwCreateData) => void;
-	onUpdateRow: (data: CiwUpdateData) => void;
+	onCreateNewRow: (data: CiwCreateData, key: string) => void;
+	onUpdateRow: (data: CiwUpdateData, key: string) => void;
 	onDeleteRow: (id: number) => void;
 };
