7154b064b3ac94bf1e54857c9bd58ccfc053e360
Mor-ald <mor.ald@yandex.ru>
2024-08-10 11:47:39 +0300

    init(components): init inputtext type

---
+++ src/components/inputs/inputtext/InputText.types.ts
@@ -0,0 +1,12 @@
+export type IInputText = {
+	field: keyof {
+		rowName: string;
+		salary: string;
+		equipmentCosts: string;
+		overheads: string;
+		estimatedProfit: string;
+	};
+	formData: { rowName: string; salary: string; equipmentCosts: string; overheads: string; estimatedProfit: string };
+	onChangeFormData: (field: string, value: string) => void;
+	onPressEnter: () => void;
+};
