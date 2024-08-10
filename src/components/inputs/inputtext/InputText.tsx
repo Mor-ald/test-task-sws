d0d894331c0437c2895e525329b17bfe4e0e86ec
Mor-ald <mor.ald@yandex.ru>
2024-08-10 11:48:19 +0300

    feature(components): add jsx, type, onkeydown method

--- src/components/inputs/inputtext/InputText.tsx
+++ src/components/inputs/inputtext/InputText.tsx
@@ -1,8 +1,24 @@
+import { useCallback } from "react";
 import styles from "../Inputs.module.scss";
+import { IInputText } from "./InputText.types";
 
 /**
  * InputText component
  */
-export default function InputText() {
-	return <></>;
+export default function InputText({ field, formData, onChangeFormData, onPressEnter }: IInputText) {
+	const onKeyDown = useCallback((e: React.KeyboardEvent<HTMLInputElement>) => {
+		if (e.key === "Enter") onPressEnter();
+	}, []);
+
+	return (
+		<>
+			<input
+				className={styles["input-text"]}
+				type="text"
+				value={formData[field]}
+				onChange={(e) => onChangeFormData(field, e.target.value)}
+				onKeyDown={onKeyDown}
+			/>
+		</>
+	);
 }
