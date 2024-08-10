837b7b91ff30c608d86dc0591e154d520a4bdab7
Mor-ald <mor.ald@yandex.ru>
2024-08-10 12:08:26 +0300

    feature(components): add new methods
    add new methods: onAddChildRow, onCreateNewRow, onUpdateRow, onDeleteRow
    add Row component

--- src/components/treerows/TreeRows.tsx
+++ src/components/treerows/TreeRows.tsx
@@ -1,15 +1,29 @@
+import { CiwCreateData, CiwUpdateData } from "../../ts/ApiData";
 import { TreeNode } from "../../ts/TreeNode";
 import { Row } from "../row/Row";
 
 type ITreeNode = {
 	nodes: TreeNode[];
+	onAddChildRow: (id: number) => void;
+	onCreateNewRow: (data: CiwCreateData) => void;
+	onUpdateRow: (data: CiwUpdateData) => void;
+	onDeleteRow: (id: number) => void;
 };
 
-export function TreeRows({ nodes }: ITreeNode) {
+export function TreeRows({ nodes, onAddChildRow, onCreateNewRow, onUpdateRow, onDeleteRow }: ITreeNode) {
 	const rows: JSX.Element[] = [];
 
 	const renderRows = (node: TreeNode) => {
-		rows.push(<Row key={node.key} node={node} />);
+		rows.push(
+			<Row
+				key={node.data.id}
+				node={node}
+				onCreateNewRow={onCreateNewRow}
+				onAddChildRow={onAddChildRow}
+				onUpdateRow={onUpdateRow}
+				onDeleteRow={onDeleteRow}
+			/>,
+		);
 
 		if (node.children.length > 0) {
 			node.children.forEach((node) => renderRows(node));
